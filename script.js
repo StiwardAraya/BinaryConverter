f819a88b5f0011dca802b816f9856035bfeec666
Stewart Araya Calderón <StiwardArayaCalderon98@gmail.com>
2024-08-22 18:02:18 -0600

    Change(front): documentation for the new function

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
+/**
+ * Event listener for the convert button click.
+ * Checks if the user input is a valid decimal number greater than or equal to 0.
+ * If it is, logs the user input to the console.
+ */
 const checkUserInput = () => {
   if (
     !numberInput.value ||
